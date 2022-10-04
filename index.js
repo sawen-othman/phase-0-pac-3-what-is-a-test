e3732d3a2cbc4a230947846e4759fa652eba564f
sawen <sawenothman2001@gmail.com>
2022-10-04 13:43:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,4 +2,26 @@ const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
+console.log(message);
+describe("what-is-a-test", () => {
+  describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+});
